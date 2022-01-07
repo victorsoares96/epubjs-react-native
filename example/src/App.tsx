bd16228ae457d1b577cafd0b56c4167d58c3b779
Victor Soares <vitorsoares96@hotmail.com>
2022-01-07 13:49:44 -0300

    modified:   example/src/App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -7,34 +7,7 @@ export default function App() {
   const { width, height } = useWindowDimensions();
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <BookProvider
-        defaultTheme={{
-          'fontSize': '100%',
-          'body': {
-            'background': '#e8dcb8',
-            'font-size': '100%',
-          },
-          'p': {
-            'color': '#000 !important',
-            'font-size': '100%',
-          },
-          'li': {
-            'color': '#000 !important',
-            'font-size': '100%',
-          },
-          'h1': {
-            color: '#000 !important',
-          },
-          'a': {
-            'color': '#000 !important',
-            'pointer-events': 'none',
-            'cursor': 'default',
-          },
-          '::selection': {
-            background: 'lightskyblue',
-          },
-        }}
-      >
+      <BookProvider>
         <Reader
           src="https://s3.amazonaws.com/moby-dick/OPS/package.opf"
           width={width}
