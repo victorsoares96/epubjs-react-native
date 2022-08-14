d6e028af7c2581f1a59faffa088b2b77ac2c0fab
Victor Soares <vitorsoares96@hotmail.com>
2022-08-14 00:17:10 -0300

    :white_check_mark: tests(jest.setup.ts): increase tests timeout

--- jest.setup.ts
+++ jest.setup.ts
@@ -4,6 +4,8 @@ import '@testing-library/jest-native/extend-expect';
 
 import 'react-native-gesture-handler/jestSetup';
 
+jest.setTimeout(20000);
+
 jest.mock('react-native-webview', () => {
   const WebView = require('./src/mocks/react-native-webview');
 
