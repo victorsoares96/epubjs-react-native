660bb1202f190ab26745b0c2eefd4b393356bf16
Victor Soares <vitorsoares96@hotmail.com>
2024-04-12 19:52:05 -0300

    modified:   example-bare/examples/Basic/index.tsx 	modified:   example-expo/examples/Basic/index.tsx 	modified:   tsconfig.json

--- example-bare/examples/Basic/index.tsx
+++ example-bare/examples/Basic/index.tsx
@@ -8,15 +8,6 @@ export function Basic() {
       <Reader
         src="https://s3.amazonaws.com/moby-dick/OPS/package.opf"
         fileSystem={useFileSystem}
-        onPress={() => console.log('onPress')}
-        onSingleTap={() => console.log('onSingleTap')}
-        onDoublePress={() => console.log('onDoublePress')}
-        onDoubleTap={() => console.log('onDoubleTap')}
-        onLongPress={() => console.log('onLongPress')}
-        onSwipeLeft={() => console.log('onSwipeLeft')}
-        onSwipeRight={() => console.log('onSwipeRight')}
-        onSwipeUp={() => console.log('onSwipeUp')}
-        onSwipeDown={() => console.log('onSwipeDown')}
       />
     </ReaderProvider>
   );
